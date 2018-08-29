0a929ccd1f25cd2af68ed4397b4226cec3f56a4f
Adrian Ciura <aciura@gmail.com>
2018-08-29 18:39:33 +0200

    Initial version

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=terminator0-Web-Deploy.pubxml -p:Password=d6kG0eeKrt8W7Snl0mgdqdCE2P5rLCgfzBNatosGQLjTLehL4joRAYz4rBkT
+
