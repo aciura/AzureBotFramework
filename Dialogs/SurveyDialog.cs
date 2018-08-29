152ecfd6a254569533b70bb8ce416bcfeb3e6379
Adrian Ciura <aciura@gmail.com>
2018-08-29 23:19:42 +0200

    Add SurveyDialog

---
+++ Dialogs/SurveyDialog.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace SimpleEchoBot.Dialogs
+{
+    [Serializable]
+    public class SurveyDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            PromptDialog.Text(context, TrainingNameReceivedAsync, "What was the training name?");
+        }
+
+        private async Task TrainingNameReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            PromptDialog.Choice(context, DidYouLikeReceivedAsync, 
+                new[] { "Yes", "Ehh", "No" }, 
+                "Did you like the training?");
+        }
+
+        private Task DidYouLikeReceivedAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
